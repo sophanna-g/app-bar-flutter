60104201e4bc304bc6c31b7382ed465c4f73659d
sophanna-g <sophanna.g@gmail.com>
2024-11-24 12:19:54 +0700

    Add debugShowCheckedModeBanner: false

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: AppBarScreen(),
     );
   }
